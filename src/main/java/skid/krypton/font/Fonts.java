de1174fffffc03c98c4b3e9758f91c8fb7c2cbb9
PedroBeSquirtin <connorjackhughes011@gmail.com>
2026-03-27 22:15:12 +0000

    Update Fonts.java

--- src/main/java/skid/krypton/font/Fonts.java
+++ src/main/java/skid/krypton/font/Fonts.java
@@ -1,5 +1,8 @@
 package skid.krypton.font;
 
 public final class Fonts {
-    public static GlyphPageFontRenderer FONT = GlyphPageFontRenderer.init("/font.ttf", 35, false, false, false);
+    // Using a modern, clean font - you can change the path to your custom font file
+    public static GlyphPageFontRenderer FONT = GlyphPageFontRenderer.init("/assets/uranium/fonts/Inter-Regular.ttf", 18, false, false, false);
+    public static GlyphPageFontRenderer TITLE_FONT = GlyphPageFontRenderer.init("/assets/uranium/fonts/Inter-Bold.ttf", 24, false, false, false);
+    public static GlyphPageFontRenderer SMALL_FONT = GlyphPageFontRenderer.init("/assets/uranium/fonts/Inter-Regular.ttf", 14, false, false, false);
 }
