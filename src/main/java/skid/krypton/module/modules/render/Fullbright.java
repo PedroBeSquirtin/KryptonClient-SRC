ba822c3477cde67946f4c339a68c574f1076b1a8
PedroBeSquirtin <connorjackhughes011@gmail.com>
2026-03-28 14:49:37 +0000

    Update Fullbright.java

--- src/main/java/skid/krypton/module/modules/render/Fullbright.java
+++ src/main/java/skid/krypton/module/modules/render/Fullbright.java
@@ -1,47 +1,6 @@
-package skid.krypton.module.modules.render;
-
-import skid.krypton.event.EventListener;
-import skid.krypton.event.events.TickEvent;
-import skid.krypton.module.Category;
-import skid.krypton.module.Module;
-
 public final class Fullbright extends Module {
 
-    private double oldGamma = -1;
-
     public Fullbright() {
-        super("Fullbright", "Brightens the world", -1, Category.RENDER);
-    }
-
-    @Override
-    public void onEnable() {
-        super.onEnable();
-
-        if (mc.player == null) return;
-
-        // Save original gamma once
-        if (oldGamma == -1) {
-            oldGamma = mc.options.getGamma().getValue();
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        super.onDisable();
-
-        if (mc.player == null) return;
-
-        // Restore gamma
-        if (oldGamma != -1) {
-            mc.options.getGamma().setValue(oldGamma);
-        }
-    }
-
-    @EventListener
-    public void onTick(TickEvent event) {
-        if (!isEnabled() || mc.player == null) return;
-
-        // Constantly force gamma
-        mc.options.getGamma().setValue(16.0); // 16 is stable + not clamped
+        super("Fullbright", "True full brightness", -1, Category.RENDER);
     }
 }
